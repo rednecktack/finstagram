78e84a65db9f1e86544101434b13e79fa6f07b91
Lori Thornburg <rednecktack@hotmail.com>
2023-08-10 00:18:27 +0000

    W3D2

--- demo.rb
+++ demo.rb
@@ -1,3 +1,17 @@
-puts "Hello"
-puts 13
-puts 5 > 12
+def yellow_pager(word)
+
+    phone_number = ""
+word.each_char do |letter|
+    if letter == "a" || letter == "b" || letter == "c"
+        phone_number += "2"
+        elsif   letter == "m"
+            phone_number += 6
+
+       else  phone_number += letter
+    end
+end  
+    puts phone_number
+
+
+    end
+yellow_pager ("abcdefgmno")
